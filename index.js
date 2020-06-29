14db360ecbb2d2284b3828c9677d78d9afa30caa
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-29 04:01:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function createEmployeeRecord(arr) {
+  return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+function createEmployeeRecords(arr) {
+  return arr.map(createEmployeeRecord);
+}
